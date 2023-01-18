dc194cdb88b470b41a4457989d5c84242ad2e4d6
Divya2603thara <117896861+Divya2603thara@users.noreply.github.com>
2023-01-18 16:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
+    //converting string to array
+    let array = str.split('');
+    //converting reverse array
+    let reverse=array.reverse();
+    //join the reverse array and mae string
+    let reversestring=reverse.join('');
+
+    if(str == reversestring){
+        return true
+    }else{
+        false
+    }
 
 }
+
 module.exports = palindrome
